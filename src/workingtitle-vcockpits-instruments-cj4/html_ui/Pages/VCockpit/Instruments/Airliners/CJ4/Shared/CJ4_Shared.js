6646f20e7b5a31806c43e459b93dff18eef93509
slip664 <72432870+slip664@users.noreply.github.com>
2021-03-13 22:36:39 -0800

    terrain updates III
    more terrain updates to include gear position based on TAWS from manual.

--- src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/Shared/CJ4_Shared.js
+++ src/workingtitle-vcockpits-instruments-cj4/html_ui/Pages/VCockpit/Instruments/Airliners/CJ4/Shared/CJ4_Shared.js
@@ -3163,23 +3163,37 @@ class CJ4_MapContainer extends NavSystemElementContainer {
             const curve = new Avionics.Curve();
             const altitude = Math.min(Simplane.getAltitude(), 15000);
             const AGL = SimVar.GetSimVarValue("RADIO HEIGHT", "feet");
+            const gear = SimVar.GetSimVarValue("GEAR HANDLE POSITION", "boolean");
 
             curve.interpolationFunction = Avionics.CurveTool.StringColorRGBInterpolation;
             curve.add(0, '#000000');
-            if (AGL <= 1000) {
-                curve.add(altitude - 1, '#ffe017');             
-                curve.add(altitude - 250, '#ffe017');
-                curve.add(altitude - 500, '#000000');
+            if ((AGL <= 250) && (gear)) {
+                curve.add(altitude, '#000000')
+                if (AGL <= 250) {
+                    curve.add(altitude - 250, '#000000');
+                }
+            } else if ((AGL <= 1000) && (AGL > 250)) {
+                curve.add(altitude - 1, '#ffe017');
+                if (gear) {    
+                    curve.add(altitude - 250, '#000000');
+                    } else {
+                        curve.add(altitude - 250, '#ffe017');
+                        curve.add(altitude - 500, '#000000');
+                }
             } else if ((AGL > 1000) && (AGL <= 2000)) {
-                curve.add(altitude - 1, '#5cdb37');
-                curve.add(altitude - 500, '#5cdb37');
-                curve.add(altitude - 1000, '#000000');
+                curve.add(altitude + 5, '#5cdb37');
+                if (gear) {
+                    curve.add(altitude - 500, '#000000');
+                    } else {
+                        curve.add(altitude - 500, '#5cdb37');
+                        curve.add(altitude - 1000, '#000000');
+                }
             } else if (AGL > 2000) {
                 curve.add(altitude - 1, '#5cdb37');
                 curve.add(altitude - 500, '#5cdb37');
                 curve.add(altitude - 1000, '#000000');
             }
-            curve.add(altitude, '#ffe017');
+            curve.add(altitude + 5, '#ffe017');
             curve.add(altitude + 500, '#ffe017');
             curve.add(altitude + 1999, '#ffe017');
             curve.add(altitude + 2000, '#ff0000');
